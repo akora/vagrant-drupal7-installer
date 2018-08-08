abdfb1798ceee222b57aca793cae819e124fe2d5
Andras Kora <ak@akora.info>
2018-08-08 23:58:21 +0200

    SendGrid support

---
+++ install-composer-sendgrid.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "=== Installing missing system components..."
+sudo apt-get install -y zip unzip
+
+echo "=== Installing Composer..."
+cd ~
+curl -sS https://getcomposer.org/installer | php
+sudo mv composer.phar /usr/local/bin/composer
+
+echo "=== Installing modules for SendGrid integration..."
+cd /var/www/html
+sudo chmod 777 /home/vagrant/.drush/cache/default
+drush dl xautoload mailsystem sendgrid_integration -y
+drush en xautoload -y
+
+echo "=== Enabling SendGrid integration..."
+cd sites/all/modules/sendgrid_integration/
+composer install
+cd /var/www/html
+drush en mailsystem sendgrid_integration -y
+
+exit 0
